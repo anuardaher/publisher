c2c69a915b594d9bb788950a1fa049b95bc013d4
Anuar <anuar_daher@hotmail.com>
2019-10-04 00:05:13 -0300

    limpando cache do git

--- api/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.use('/api/v1/users', require('./users.route'));
-router.use('/api/v1/articles', require('./articles.route'));
-router.use('/api/v1/tags', require('./tags.route'));
-
-router.use('/api/v1/auth', require('./auth'));
-
-module.exports = router;
--- app/plugins/socialSharing.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import socialSharing from 'vue-social-sharing'
-
-Vue.use(socialSharing)
